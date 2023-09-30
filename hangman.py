c12bb9e594a24f73d07d5cb7b2735d2e7860f09d
RoyBS1 <roy.b.samuel@gmail.com>
2023-09-30 22:48:21 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import re
 print("Welcome to the game Hangman")
 HANGMAN_ASCII_ART = "HANGMAN"
 print(HANGMAN_ASCII_ART)
@@ -5,8 +6,11 @@ MAX_TRIES = 6
 print("\n")
 print(MAX_TRIES)
 
-word = "yellow"
+word = input()
+word = word.lower()
+print(word)
+print(re.sub('[a-z]', '_', word))
+
 print("Guess a letter:")
 letter = input()
-print(letter.lower())
 print(letter in word)

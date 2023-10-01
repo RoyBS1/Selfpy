d8852dbc310a144f33482b990ac460cdaa4c81d2
RoyBS1 <roy.b.samuel@gmail.com>
2023-10-01 13:44:31 +0300

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,13 +4,22 @@ HANGMAN_ASCII_ART = "HANGMAN"
 print(HANGMAN_ASCII_ART)
 MAX_TRIES = 6
 print("\n")
-print(MAX_TRIES)
+print("Maximum number of tries is: " + str(MAX_TRIES))
 
+print("Please insert a word:\n")
 word = input()
 word = word.lower()
-print(word)
+print("The word is " + word)
 print(re.sub('[a-z]', '_', word))
 
 print("Guess a letter:")
 letter = input()
+if(len(letter) > 1) and (not letter.isalpha()) :
+    print("E3")
+elif(not letter.isalpha()):
+    print("E2")
+elif(len(letter) > 1):
+    print("E1")
+    
+print("The letter is in the word: ")
 print(letter in word)

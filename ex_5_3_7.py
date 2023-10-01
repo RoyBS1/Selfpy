5ddff974fd5e545fe45b5ce2cd54e1e22472a4c2
RoyBS1 <roy.b.samuel@gmail.com>
2023-10-01 17:02:52 +0300

    Create ex_5_3_7.py

---
+++ ex_5_3_7.py
@@ -0,0 +1,12 @@
+def chocolate_maker(small, big, x):
+    if (small + 5*big >= x):
+        print("True")
+    else:
+        print("False")
+    
+    
+chocolate_maker(3, 1, 8)
+
+chocolate_maker(3, 1, 9)
+
+chocolate_maker(3, 2, 10)

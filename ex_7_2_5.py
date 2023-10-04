cee99c11db680275a9cba5b5854e781b5ce62462
RoyBS1 <roy.b.samuel@gmail.com>
2023-10-04 13:44:19 +0300

    Create ex_7_2_5.py

---
+++ ex_7_2_5.py
@@ -0,0 +1,16 @@
+def sequence_del(my_str):
+    my_str2 = []
+    for x in my_str:
+        if x not in my_str2:
+            my_str2.append(x)
+            #print("ynooooooooo")
+    print(*my_str2)
+
+
+
+
+sequence_del("ppyyyyythhhhhooonnnnn")
+
+sequence_del("SSSSsssshhhh")
+
+sequence_del("Heeyyy   yyouuuu!!!")
